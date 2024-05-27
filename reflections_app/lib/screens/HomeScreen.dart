1395b0a411c8520bf2d293ef14c18ab88ecdd9ce
richardljohn <richardljohn@gmail.com>
2024-05-26 23:56:52 -0400

    Start

---
+++ reflections_app/lib/screens/HomeScreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child:Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            IconButton(
+              onPressed: (){},
+              iconSize: 30.0,
+              icon: const Icon(
+                Icons.search_rounded,
+                ),
+              ),
+              IconButton(
+              onPressed: (){},
+              iconSize: 30.0,
+              icon: const Icon(
+                // Icons.short_text_rounded,
+                ),
+              ),
+          ],)
+        ],)
+      ),
+    );
+  }
+}
