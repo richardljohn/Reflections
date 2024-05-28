71ead9f77f62d950675f5ab28f2559d02c65cfce
richardljohn <richardljohn@gmail.com>
2024-05-27 23:40:27 -0400

    Routes

--- reflections_app/lib/main.dart
+++ reflections_app/lib/main.dart
@@ -24,7 +24,7 @@ class Reflections extends StatelessWidget {
         ),
         initialRoute: '/',
         routes: {
-          '/': (context) => LoginScreen(),
+          '/': (context) => Signup(),
           '/signup': (context) => SignupScreen(),
           '/home': (context) => HomeScreen(),
         });
