6d51cf088c7f2a535ee2db0c1aa7f3d8e76b9da5
richardljohn <richardljohn@gmail.com>
2024-05-27 22:38:22 -0400

    Add Button

--- reflections_app/lib/screens/HomeScreen.dart
+++ reflections_app/lib/screens/HomeScreen.dart
@@ -12,27 +12,34 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child:Column(
+          child: Column(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-            IconButton(
-              onPressed: (){},
-              iconSize: 30.0,
-              icon: const Icon(
-                Icons.search_rounded,
+              IconButton(
+                onPressed: () {},
+                iconSize: 30.0,
+                icon: const Icon(
+                  Icons.search_rounded,
                 ),
               ),
               IconButton(
-              onPressed: (){},
-              iconSize: 30.0,
-              icon: const Icon(
-                Icons.short_text_rounded,
+                onPressed: () {},
+                iconSize: 30.0,
+                icon: const Icon(
+                  Icons.short_text_rounded,
                 ),
               ),
-          ],)
-        ],)
+            ],
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+        child: const Icon(Icons.add)
       ),
     );
   }
