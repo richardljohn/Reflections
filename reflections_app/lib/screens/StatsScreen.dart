53ed7b0117befb21f8517f9eb2eba2d8130bed72
richardljohn <richardljohn@gmail.com>
2024-06-20 23:26:23 -0400

    Routes

--- reflections_app/lib/screens/StatsScreen.dart
+++ reflections_app/lib/screens/StatsScreen.dart
@@ -6,6 +6,8 @@ class StatsScreen extends StatefulWidget {
 }
 
 class _StatsScreenState extends State<StatsScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
