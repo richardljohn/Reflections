b6e7015001bb518c2f8be7fe0e5648c0d6ab01f3
richardljohn <richardljohn@gmail.com>
2024-06-20 23:25:38 -0400

    Routes

--- reflections_app/lib/screens/StatsScreen.dart
+++ reflections_app/lib/screens/StatsScreen.dart
@@ -19,42 +19,6 @@ class _StatsScreenState extends State<StatsScreen> {
       appBar: AppBar(
         title: Text('Your Stats'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _login,
-              child: Text('Login'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
-              child: Text('Don\'t have an account? Sign Up'),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
