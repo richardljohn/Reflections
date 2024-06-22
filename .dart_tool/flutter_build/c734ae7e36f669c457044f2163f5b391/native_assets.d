19835ff7852b6ac5c72da2a1f0450b9cd6d92141
richardljohn <richardljohn@gmail.com>
2024-06-22 13:15:50 -0400

    Redp

--- .dart_tool/flutter_build/c734ae7e36f669c457044f2163f5b391/native_assets.d
+++
@@ -1,1 +0,0 @@
- /Users/richardjohn/Desktop/Reflections/.dart_tool/flutter_build/c734ae7e36f669c457044f2163f5b391/native_assets.yaml: 
--- reflections_app/android/app/src/main/kotlin/com/example/reflections_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.reflections_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
