7e0cb0fdb8999d9bfe33125b48bd4251ec96ff98
richardljohn <richardljohn@gmail.com>
2024-06-22 11:59:24 -0400

    Firebase Configuration Auth

--- reflections_app/lib/pages/auth.dart
+++ reflections_app/lib/pages/auth.dart
@@ -6,4 +6,28 @@ class Auth {
   User? get currentUser => _firebaseAuth.currentUser;
 
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signout() async {
+    await _firebaseAuth.signOut();
+  }
 }
